ca94eeb2c98b60b79009518a233008823fdc22e1
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2023-04-26 19:14:50 +0300

    Implement recursive directory scanning

---
+++ src/main/kotlin/com/github/unix_junkie/dependency_scanner/ExitCode.kt
@@ -0,0 +1,7 @@
+package com.github.unix_junkie.dependency_scanner
+
+enum class ExitCode {
+	NO_ERROR,
+	ILLEGAL_ARGS,
+	PACKAGE_ROOT_NONEXISTENT,
+}
--- src/main/kotlin/com/github/unix_junkie/dependency_scanner/Main.kt
+++ src/main/kotlin/com/github/unix_junkie/dependency_scanner/Main.kt
@@ -2,6 +2,66 @@
 
 package com.github.unix_junkie.dependency_scanner
 
-fun main() {
-	println("java.version = " + System.getProperty("java.version"))
+import com.github.unix_junkie.dependency_scanner.ExitCode.ILLEGAL_ARGS
+import org.apache.tika.Tika
+import org.apache.tika.config.TikaConfig
+import java.io.File
+import kotlin.system.exitProcess
+
+private val className = {}.javaClass.enclosingClass.name
+
+fun main(vararg args: String) {
+	if (args.size != 1) {
+		usage()
+		exitProcess(ILLEGAL_ARGS)
+	}
+
+	val packageRoot = File(args[0])
+
+	when {
+		packageRoot.isDirectory -> scanPackage(packageRoot)
+
+		else -> {
+			println("Not a directory: $packageRoot")
+			exitProcess(ExitCode.PACKAGE_ROOT_NONEXISTENT)
+		}
+	}
 }
+
+private fun scanPackage(packageRoot: File) {
+	val tika = Tika(TikaConfig.getDefaultConfig())
+
+	scanDirectory(packageRoot).forEach { file ->
+		val type = tika.detect(file)
+		println("$file -> $type")
+	}
+}
+
+/**
+ * @return the sequence of regular files which are children of [directory].
+ */
+private fun scanDirectory(directory: File): Sequence<File> {
+	check(directory.isDirectory)
+
+	val children = directory.listFiles()
+
+	check(children != null)
+
+	return sequence {
+		yieldAll(children.asSequence()
+				.filter(File::isDirectory)
+				.sortedBy(File::getName)
+				.flatMap(::scanDirectory))
+
+		yieldAll(children.asSequence()
+				.filter(File::isFile)
+				.sortedBy(File::getName))
+	}
+}
+
+private fun usage() {
+	println("Usage: $className [DIRECTORY]")
+}
+
+private fun exitProcess(exitCode: ExitCode): Nothing =
+		exitProcess(exitCode.ordinal)
